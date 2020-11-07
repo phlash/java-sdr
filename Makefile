d20aa2a5e741b67dae89a45dc1444325e1fffd2d
Phil Ashby <phil.github@ashbysoft.com>
2020-11-07 20:02:22 +0000

    Performance fix for waterfall, thanks to https://github.com/DSheirer/sdrtrunk :)

--- Makefile
+++ Makefile
@@ -78,4 +78,4 @@ $(OUTDEPS):
 
 # Try it!
 test: all
-	java -jar $(OUT)/jsdr.jar
+	java -jar $(OUT)/jsdr.jar -run
--- waterfall.java
+++ waterfall.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.image.MemoryImageSource;
+import java.awt.image.ColorModel;
 import javax.swing.JPanel;
 
 public class waterfall extends JPanel implements IPublishListener {
     private ILogger logger;
-	private ArrayList<double[]> data;
     private AudioDescriptor adsc;
     private Color peak = Color.CYAN;
-    private BufferedImage buf;
+	private ArrayList<double[]> data;
+    private int[] pixs;
+    private MemoryImageSource mis;
+    private Image img;
     private boolean needpaint = true;
+
     public waterfall(IPublish pub, ILogger log, IAudio aud) {
         logger= log;
         data = new ArrayList<double[]>();
@@ -36,71 +41,78 @@ public class waterfall extends JPanel implements IPublishListener {
             synchronized(this) {
                 adsc = ((IAudio)val).getAudioDescriptor();
                 data.clear();
+                pixs = null;
             }
             needpaint = true;
         }
 	}
 
     protected synchronized void paintComponent(Graphics g) {
-		// skip if doesn't need painting
+		// skip if nothing need painting
 		if (!needpaint)
 			return;
         long stime = System.nanoTime();
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
-        // Bail if no data
+        // bail if no data
         if (data.size()<1)
             return;
-        // check if current image buffer is ok for size
-        if (null==buf || getWidth()!=buf.getWidth() || getHeight()!=buf.getHeight())
-            buf = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-		// Step size and FFT offset for resampling to screen size
-		float s = (float)(data.get(0).length-2)/(float)getWidth();
-		int off = getWidth()/2;
-		// adjust step and offset if only single channel data
-		if (adsc.chns<2) {
-			s = s/2;
-			off = 0;
-		}
-		int t = (int)Math.ceil(s);
         // remove old lines (if any)
         while (data.size() > getHeight())
             data.remove(data.size()-1);
-        // iterate all visible lines, find maxima across all
-        double max = 0;
-        for (double[] psd : data) {
-            if (psd[psd.length-1] > max)
-                max = psd[psd.length-1];
-        }
-        long mtime = System.nanoTime();
-        // intensity scaling to fit all psd values in 0-255
-        float h = (float)(255.0/Math.log10(max+1.0));
-        // iterate again, painting lines
-        int y = 0;
-        int cnt = 0;
-        for (double[] psd : data) {
-            //  scale to width and draw using colour intensity
-            for (int p=0; p<getWidth()-1; p++) {
-                // offset and wrap index to display negative freqs, then positives..
-                int i = (p+off) % getWidth();
-                int f = (int)(Math.log10(getMax(data.get(y), (int)(i*s), t)+1.0)*h);
-                Color c = new Color(
-                    (peak.getRed()*f/256)&0xff,
-                    (peak.getGreen()*f/256)&0xff,
-                    (peak.getBlue()*f/256)&0xff
-                );
-                buf.setRGB(p, y, c.getRGB());
-                cnt++;
+		// step size and FFT offset for resampling to screen size
+		float step = (float)(data.get(0).length-2)/(float)getWidth();
+		int off = getWidth()/2;
+		// adjust step and offset if only single channel data
+		if (adsc.chns<2) {
+			step = step/2;
+			off = 0;
+		}
+        ColorModel cm = ColorModel.getRGBdefault();
+        // check if current image is ok for size, or if we have no pixels yet
+        if (null==pixs || getWidth()!=img.getWidth(null) || getHeight()!=img.getHeight(null)) {
+            // re-create pixel data from raw
+            pixs = new int[getWidth()*getHeight()];
+            int line = 0;
+            for (double[] psd : data) {
+                paintLine(psd, pixs, line*getWidth(), step, off);
+                line++;
             }
-            y++;
+            // connect to animation image
+            mis = new MemoryImageSource(getWidth(), getHeight(), cm, pixs, 0, getWidth());
+            mis.setAnimated(true);
+            img = createImage(mis);
+        } else {
+            // copy down pixels, add top line from raw
+            System.arraycopy(pixs, 0, pixs, getWidth(), pixs.length-getWidth());
+            paintLine(data.get(0), pixs, 0, step, off);
         }
+        // push pixels!
+        mis.newPixels(pixs, cm, 0, getWidth());
         long ptime = System.nanoTime();
-        g.drawImage(buf, 0, 0, null);
+        g.drawImage(img, 0, 0, null);
         long rtime = System.nanoTime();
         needpaint = false;
-        logger.logMsg("waterfall: render (nsecs) max/lines/blt "+(mtime-stime)+"/"+(ptime-mtime)+"/"+(rtime-ptime));
+        logger.logMsg("waterfall: render (nsecs) lines/blt "+(ptime-stime)+"/"+(rtime-ptime));
     }
 
+    private void paintLine(double[] psd, int[] pixs, int start, float step, int off) {
+        // intensity scaling to fit all psd values in 0-255
+        double max = psd[psd.length-1];
+        float h = (float)(255.0/Math.log10(max+1.0));
+        //  scale to width and draw using colour intensity
+        for (int p=0; p<getWidth()-1; p++) {
+            // offset and wrap index to display negative freqs, then positives..
+            int i = (p+off) % getWidth();
+            int f = (int)(Math.log10(getMax(psd, (int)(i*step), (int)step)+1.0)*h);
+            Color c = new Color(
+                (peak.getRed()*f/256)&0xff,
+                (peak.getGreen()*f/256)&0xff,
+                (peak.getBlue()*f/256)&0xff
+            );
+            pixs[start+p] = c.getRGB();
+        }
+    }
 	// Find largest magnitude value in a array from offset o, length l, stride 1
 	private double getMax(double[]a, int o, int l) {
 		double r = 0;
